a1ef99c225d72698a6ba9962da88a516829c54fa
Alireza <alireza.tali@gmail.com>
2023-04-09 14:02:23 +0430

    create main activity

--- app/src/main/java/com/tali/websocketsample/view/activities/MainActivity.java
+++ app/src/main/java/com/tali/websocketsample/view/activities/MainActivity.java
@@ -1,16 +1,28 @@
 package com.tali.websocketsample.view.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.ListFragment;
 
 import android.os.Bundle;
 
 import com.tali.websocketsample.R;
+import com.tali.websocketsample.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class MainActivity extends BaseActivity<ActivityMainBinding> {
+
+    @Override
+    public int getLayoutRes() {
+        return R.layout.activity_main;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ListFragment fragment = new ListFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragmentContainer, fragment, "List Fragment").commit();
     }
 }
